126dabcfe7a342dd897940ecf6405009827a3877
punithgopa <143382363+punithgopa@users.noreply.github.com>
2023-09-13 07:25:38 +0530

    Add files via upload
    my project to git

---
+++ punith project/src/Student.java
@@ -0,0 +1,50 @@
+public class Student {
+    public static void main(String[] args) {
+
+    }
+
+    private int id;
+    private String name;
+    private String DateOfBirth;
+    private String Department;
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public String getDepartment() {
+        return Department;
+    }
+
+    // Setter methods
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateOfBirth(String DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    public void setDepartment(String Department) {
+        this.Department = Department;
+    }
+
+
+}
+
+
+
+
